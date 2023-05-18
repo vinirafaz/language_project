73efea035547c0f6190cd7aaf63b4ff1aa31a80e
Vinicius Azevedo <vinirafa0306@gmail.com>
2023-05-18 19:58:17 -0300

    Estruturando o app

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import './Header.css';
+
+export const Header = () => {
+    return( 
+        <header className="header">
+            <img src='./imgs/logo192.png' alt='Logo Bside'/>
+            Teste
+        </header>
+    );
+}
